821e4f5144672ddd5c24d0f00432edb650a00537
Marius <marius.pop94@gmail.com>
2017-03-25 15:03:19 -0600

    serverside added

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Marius on 3/25/2017.
+ */
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
+
+http.listen( port, function () {
+    console.log('listening on port', port);
+});
+
+app.use(express.static(__dirname + '/public'));
