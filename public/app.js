e2d2d728371f2aea1b47a2bacca038d9094e578f
Jorge <jorge_gomez12@hotmail.com>
2017-03-25 13:15:48 -0600

    login works with firebase

---
+++ public/app.js
@@ -0,0 +1,64 @@
+(function() {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCiTqWN0OhnaaQibMzHgL5itpqQX1bnEo8",
+    authDomain: "employee-time-tracking-b561d.firebaseapp.com",
+    databaseURL: "https://employee-time-tracking-b561d.firebaseio.com",
+    storageBucket: "employee-time-tracking-b561d.appspot.com",
+    messagingSenderId: "195814535649"
+  };
+  firebase.initializeApp(config);
+  //TESTING REALTIME DATABASE
+  // var BigOne = document.getElementById('Greetings');
+  // var dbRef = firebase.database().ref().child('Employee').child('hAymr6CBameiJT6BlkofnYYChSi1').child('name');
+  // dbRef.on('value', snap => Greetings.innerText = snap.val());
+
+  //Get UI element references
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('loginButton');
+  const btnSignUp = document.getElementById('signUpButton');
+  const btnLogOut = document.getElementById('logOutButton');
+  const showUser = document.getElementById('User');
+
+  // Add login event
+
+  btnLogin.addEventListener('click', e => {
+
+      // Get email and password values
+      const email = txtEmail.value;
+      //console.log(email);
+      const password = txtPassword.value;
+      //console.log(password);
+      const auth = firebase.auth();
+
+      //Sign In
+      const promise = auth.signInWithEmailAndPassword(email,password);
+      promise.catch(e => console.log(e.message));
+
+
+  });
+
+  btnLogOut.addEventListener('click', e =>{
+
+    firebase.auth().signOut();
+
+
+
+  });
+
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser){
+      console.log(firebaseUser);
+      showUser.innerText = firebaseUser.email;
+      btnLogOut.classList.remove('hide');
+    } else{
+      btnLogOut.classList.add('hide');
+      showUser.innerText = 'No user.';
+    }
+
+
+  });
+
+}());
