49dface92cbb5ed6ed5c2fe969009ba701c58fba
delacost <daniel.e.lacoste@gmail.com>
2017-03-30 00:59:00 -0600

    sign-up/cancel btns now toggle, tmp navbar, sign-up form fxnality for new users

--- public/app.js
+++ public/app.js
@@ -20,36 +20,70 @@
   const txtEmail = document.getElementById('txtEmail');
   const txtPassword = document.getElementById('txtPassword');
   const btnLogin = document.getElementById('loginButton');
+  const btnSubmit = document.getElementById('submitButton');
   const btnSignUp = document.getElementById('signUpButton');
+  const btnCancel = document.getElementById('cancelButton');
   const btnLogOut = document.getElementById('logOutButton');
   const showUser = document.getElementById('User');
+  const navbar = document.getElementById('navbar');
 
   // Add login event
   btnLogin.addEventListener('click', e => {
-    // Get email and password values
     const email = txtEmail.value;
     const password = txtPassword.value;
     const auth = firebase.auth();
-    console.log(email);
-    console.log(password);
+    //console.log(email);
+    //console.log(password);
 
     // Sign In
     const promise = auth.signInWithEmailAndPassword(email,password);
     promise.catch(e => console.log(e.message));
+
+//    window.location.href = "main.html";
+//    var user = firebase.auth().currentUser;
+//    console.log(user)
   });
 
-  btnLogOut.addEventListener('click', e =>{
+  // Display the sign-up form 
+  btnSignUp.addEventListener('click', e => {
+    btnLogin.classList.add('hide');
+    btnSignUp.classList.add('hide');
+    btnCancel.classList.remove('hide');
+    btnSubmit.classList.remove('hide');
+  });
+
+  // Display the login form
+  btnCancel.addEventListener('click', e => {
+    btnLogin.classList.remove('hide');
+    btnSignUp.classList.remove('hide');
+    btnCancel.classList.add('hide');
+    btnSubmit.classList.add('hide');
+  });
+
+  // Create a new user - upon creation, the user is logged in
+  btnSubmit.addEventListener('click', e => {
+    const email = txtEmail.value;
+    const password = txtPassword.value;
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(email,password);
+    promise.catch(e => console.log(e.message));
+  });
+
+  btnLogOut.addEventListener('click', e => {
     firebase.auth().signOut();
   });
 
+  // Firebase authentication listener
   firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser){
+    if(firebaseUser) {
       console.log(firebaseUser);
       showUser.innerText = firebaseUser.email;
       btnLogOut.classList.remove('hide');
-    } else{
+      navbar.style.visibility = "visible";
+    } else {
       btnLogOut.classList.add('hide');
-      showUser.innerText = 'No user.';
+      showUser.innerText = 'no user is logged in';
+      navbar.style.visibility = "hidden";
     }
   });
 }());
