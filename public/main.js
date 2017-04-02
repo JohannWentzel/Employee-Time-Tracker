44a59aba142078b6c5e7ae9d9828963bd4cc384d
Marius <marius.pop94@gmail.com>
2017-04-02 14:01:45 -0600

    fixed imports

--- public/main.js
+++ public/main.js
@@ -38,6 +38,6 @@ btnLogOut.addEventListener('click', e => {
 function init() {
     scheduler.config.xml_date="%Y-%m-%d %H:%i";
     scheduler.init('scheduler_here',new Date(2018,0,1),"week");
-    scheduler.load("./data/events.xml");
+   // scheduler.load("./data/events.xml");
 
 }
