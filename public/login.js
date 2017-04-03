5b3ff1824a5ea9dd07bca62cc35ae1d91fe50225
delacost <daniel.e.lacoste@gmail.com>
2017-04-02 18:28:21 -0600

    fixed tab formatting, my bad

--- public/login.js
+++ public/login.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyCiTqWN0OhnaaQibMzHgL5itpqQX1bnEo8",
-	authDomain: "employee-time-tracking-b561d.firebaseapp.com",
-	databaseURL: "https://employee-time-tracking-b561d.firebaseio.com",
-	storageBucket: "employee-time-tracking-b561d.appspot.com",
-	messagingSenderId: "195814535649"
+  apiKey: "AIzaSyCiTqWN0OhnaaQibMzHgL5itpqQX1bnEo8",
+  authDomain: "employee-time-tracking-b561d.firebaseapp.com",
+  databaseURL: "https://employee-time-tracking-b561d.firebaseio.com",
+  storageBucket: "employee-time-tracking-b561d.appspot.com",
+  messagingSenderId: "195814535649"
 };
 
 firebase.initializeApp(config);
@@ -25,56 +25,56 @@ const navbar = document.getElementById('navbar');
 
 // Add login event
 btnLogin.addEventListener('click', e => {
-	const email = txtEmail.value;
-	const password = txtPassword.value;
-	const auth = firebase.auth();
-	//console.log(email);
-	//console.log(password);
+  const email = txtEmail.value;
+  const password = txtPassword.value;
+  const auth = firebase.auth();
+  //console.log(email);
+  //console.log(password);
 
-	// Sign In
-	auth.signInWithEmailAndPassword(email,password).catch( e => {
-		var errorCode = e.code;
-		var errorMessage = e.message;
-		$('#loginAlert').show();
-	});
+  // Sign In
+  auth.signInWithEmailAndPassword(email,password).catch( e => {
+    var errorCode = e.code;
+    var errorMessage = e.message;
+    $('#loginAlert').show();
+  });
 });
 
 // Display the sign-up form 
 btnSignUp.addEventListener('click', f => {
-	btnLogin.classList.add('hide');
-	btnSignUp.classList.add('hide');
-	btnCancel.classList.remove('hide');
-	btnSubmit.classList.remove('hide');
+  btnLogin.classList.add('hide');
+  btnSignUp.classList.add('hide');
+  btnCancel.classList.remove('hide');
+  btnSubmit.classList.remove('hide');
 });
 
 // Display the login form
 btnCancel.addEventListener('click', f => {
-	btnLogin.classList.remove('hide');
-	btnSignUp.classList.remove('hide');
-	btnCancel.classList.add('hide');
-	btnSubmit.classList.add('hide');
+  btnLogin.classList.remove('hide');
+  btnSignUp.classList.remove('hide');
+  btnCancel.classList.add('hide');
+  btnSubmit.classList.add('hide');
 });
 
 // Create a new user - upon creation, the user is logged in
 btnSubmit.addEventListener('click', e => {
-	const email = txtEmail.value;
-	const password = txtPassword.value;
-	const auth = firebase.auth();
-	const promise = auth.createUserWithEmailAndPassword(email,password);
-	promise.catch(e => console.log(e.message));
+  const email = txtEmail.value;
+  const password = txtPassword.value;
+  const auth = firebase.auth();
+  const promise = auth.createUserWithEmailAndPassword(email,password);
+  promise.catch(e => console.log(e.message));
 });
 
 // Firebase authentication listener
 firebase.auth().onAuthStateChanged(firebaseUser => {
-	if (firebaseUser) {
-		console.log(firebaseUser);
-		window.location.href = "main.html";
-	} else {
-		console.log(firebaseUser);
-	}
+  if (firebaseUser) {
+    console.log(firebaseUser);
+    window.location.href = "main.html";
+  } else {
+    console.log(firebaseUser);
+  }
 });
 
 // On click, hide the specified element 
 $("[data-hide]").click(function(){
-	$($(this).attr('data-hide')).hide();
+  $($(this).attr('data-hide')).hide();
 });
--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyCiTqWN0OhnaaQibMzHgL5itpqQX1bnEo8",
-	authDomain: "employee-time-tracking-b561d.firebaseapp.com",
-	databaseURL: "https://employee-time-tracking-b561d.firebaseio.com",
-	storageBucket: "employee-time-tracking-b561d.appspot.com",
-	messagingSenderId: "195814535649"
+  apiKey: "AIzaSyCiTqWN0OhnaaQibMzHgL5itpqQX1bnEo8",
+  authDomain: "employee-time-tracking-b561d.firebaseapp.com",
+  databaseURL: "https://employee-time-tracking-b561d.firebaseio.com",
+  storageBucket: "employee-time-tracking-b561d.appspot.com",
+  messagingSenderId: "195814535649"
 };
 
 firebase.initializeApp(config);
@@ -19,23 +19,23 @@ const showUser = document.getElementById('userEmail');
 const navbar = document.getElementById('navbar');
 
 firebase.auth().onAuthStateChanged(user => {
-	if (user) {
-		console.log(user);
-		showUser.innerText = user.email;
-		btnLogOut.classList.remove('hide');
-		navbar.style.visibility = "visible";
-	} else {
-		window.location.href = "index.html";
-	}
+  if (user) {
+    console.log(user);
+    showUser.innerText = user.email;
+    btnLogOut.classList.remove('hide');
+    navbar.style.visibility = "visible";
+  } else {
+    window.location.href = "index.html";
+  }
 });
 
 // Log out event
 btnLogOut.addEventListener('click', e => {
-	firebase.auth().signOut();
-	window.location.href = "index.html";
+  firebase.auth().signOut();
+  window.location.href = "index.html";
 });
 
 // Initialize the DHTMLX scheduler
 function init() {
-	scheduler.init('scheduler_here',new Date(),"month");
+  scheduler.init('scheduler_here',new Date(),"month");
 }
