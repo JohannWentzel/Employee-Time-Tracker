96d9afbacb14fe111fa14515b6b726543b3f729c
delacost <daniel.e.lacoste@gmail.com>
2017-04-02 20:09:37 -0600

    confirm password, tab formatting (again), alerts improved

--- public/login.js
+++ public/login.js
@@ -17,24 +17,28 @@ firebase.initializeApp(config);
 // Get UI element references
 const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
+const txtConfirmPassword = document.getElementById('txtConfirmPassword');
 const btnLogin = document.getElementById('loginButton');
 const btnSubmit = document.getElementById('submitButton');
 const btnSignUp = document.getElementById('signUpButton');
 const btnCancel = document.getElementById('cancelButton');
 const navbar = document.getElementById('navbar');
+const formGroup = document.getElementById('form-group');
+const alertMsg = document.getElementById('alertMsg');
+
+txtEmail.focus();
 
 // Add login event
 btnLogin.addEventListener('click', e => {
   const email = txtEmail.value;
   const password = txtPassword.value;
   const auth = firebase.auth();
-  //console.log(email);
-  //console.log(password);
 
   // Sign In
   auth.signInWithEmailAndPassword(email,password).catch( e => {
     var errorCode = e.code;
     var errorMessage = e.message;
+    alertMsg.innerHTML = "<strong>Whoops!</strong> Invalid login credentials.";
     $('#loginAlert').show();
   });
 });
@@ -45,6 +49,7 @@ btnSignUp.addEventListener('click', f => {
   btnSignUp.classList.add('hide');
   btnCancel.classList.remove('hide');
   btnSubmit.classList.remove('hide');
+  txtConfirmPassword.classList.remove('hide');
 });
 
 // Display the login form
@@ -53,12 +58,21 @@ btnCancel.addEventListener('click', f => {
   btnSignUp.classList.remove('hide');
   btnCancel.classList.add('hide');
   btnSubmit.classList.add('hide');
+  txtConfirmPassword.classList.add('hide');
 });
 
 // Create a new user - upon creation, the user is logged in
 btnSubmit.addEventListener('click', e => {
   const email = txtEmail.value;
   const password = txtPassword.value;
+  const confirmPassword = txtConfirmPassword.value;
+
+  if (password != confirmPassword) {
+    alertMsg.innerHTML = "<strong>Uhoh!</strong> Passwords do not match.";
+    $('#loginAlert').show();
+    return;
+  }
+
   const auth = firebase.auth();
   const promise = auth.createUserWithEmailAndPassword(email,password);
   promise.catch(e => console.log(e.message));
@@ -77,4 +91,16 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
 // On click, hide the specified element 
 $("[data-hide]").click(function(){
   $($(this).attr('data-hide')).hide();
+});
+
+// Pressing "Enter" now clicks "login"
+formGroup.addEventListener('keypress', e => {
+  if (e.which == 13 || e.which == 3) {
+    if ($(btnLogin).is(":visible")) {
+      btnLogin.click();
+    }
+    else {
+      btnSubmit.click();
+    }
+	}
 });
